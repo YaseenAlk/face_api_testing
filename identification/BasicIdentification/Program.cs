a97ac5e6f3affe3a7eac85de782ba91de0e63e46
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-05-31 15:28:49 -0400

    Start Basic Identification Code (step 1 done; other steps WIP)

---
+++ identification/BasicIdentification/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+
+namespace CSHttpClientSample
+{
+    static class Program
+    {
+        // Replace <Subscription Key> with your valid subscription key.
+        const string subscriptionKey = "abe02e5cbec341c195ce55750e8b0765";
+
+        // NOTE: You must use the same region in your REST call as you used to
+        // obtain your subscription keys. For example, if you obtained your
+        // subscription keys from westus, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the westcentralus region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        const string uriBase =
+            "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/";
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("First, I need to create the PersonGroup. (only needs to be done the first time)");
+            CreatePersonGroup();
+            Console.ReadLine();
+            //Console.WriteLine("Then, I need to define the Persons in the PersonGroup.");
+            //DefinePersonsInPersonGroup();
+            //Console.ReadLine();
+            //Console.WriteLine("Next, I need to detect + add faces to each Person in the PersonGroup.");
+            //DefineFacesForPersons();
+            //Console.WriteLine("Finally, I need to train the PersonGroup.");
+            //TrainPersonGroup();
+
+            //Console.WriteLine("The PersonGroup is now ready to start identifying!");
+        }
+
+        //Goal: https://[location].api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}
+        static async void CreatePersonGroup()
+        {
+            string personGroupId = "sample_group";
+            string personGroupName = "Person Group using the Sample Data";
+            string URI = uriBase + "persongroups/" + personGroupId;// + "?";
+            string reqBodyJSON = "{'name': '" + personGroupName +  "'}";
+            byte[] reqBody = Encoding.UTF8.GetBytes(reqBodyJSON);
+
+            MakeRequest("Creating PersonGroup", URI, reqBody, "application/json");
+        }
+
+        static async void MakeRequest(string purpose, string uri, byte[] reqBodyData, string bodyContentType, Dictionary<string, string> requestParameters = null)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            if (requestParameters != null)
+            {
+                foreach (string key in requestParameters.Keys)
+                {
+                    queryString[key] = requestParameters[key];
+                }
+            }
+
+            var fullUri = uri + queryString;
+
+            Console.WriteLine("Full URI: " + fullUri);  // debug line
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = reqBodyData;
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue(bodyContentType);
+                response = await client.PutAsync(fullUri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse for " + purpose + ":\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+                Console.WriteLine("\nPress Enter to exit...");  //debug line
+            }
+        }
+
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
