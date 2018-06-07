ff8b2744f56afeb3053b9378508371e012db8ca7
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-06-07 17:38:05 -0400

    Create CustomTraining project

---
+++ identification/CustomTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
