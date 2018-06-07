72c20f596e75b7856793544f123ae837ba228394
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-06-07 13:59:59 -0400

    Make new project for SampleIdentification

---
+++ identification/SampleIdentification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleIdentification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
