55a228058b6003ef6ed6a0b98f8e53929b4eb27d
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-06-11 12:45:47 -0400

    Make a new skeleton project for labelling

---
+++ labelling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace labelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
